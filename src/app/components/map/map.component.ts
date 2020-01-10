0bb871d33868646a4924c5651b48f3d8a92ba136
blockinblock <sebastblock@gmail.com>
2020-01-10 12:10:30 +0100

    Add Power Stations WFS layer

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,13 +1,23 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-import { defaults as defaultControls } from 'ol/control';
+// OpenLayers
+import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
 import ZoomToExtent from 'ol/control/ZoomToExtent';
-import { fromLonLat } from 'ol/proj';
+import { defaults as defaultControls } from 'ol/control';
+import TileLayer from 'ol/layer/Tile';
+import { Vector } from 'ol/layer';
 import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import { bbox as bboxStrategy } from 'ol/loadingstrategy';
+import { Fill, Stroke, Circle, Style } from 'ol/style';
+
+// Reprojections and conversions
+import proj4 from 'proj4';
+import { register } from 'ol/proj/proj4';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-map',
@@ -22,25 +32,62 @@ export class MapComponent implements AfterViewInit {
   constructor() {  }
 
   ngAfterViewInit() {
-    // https://stackoverflow.com/questions/48283679/use-openlayers-4-with-angular-5
+    // Define and register projection
+    proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs');
+    register(proj4);
+
+    // Create a new vector source
+    const vectorSource = new VectorSource({
+      format: new GeoJSON({}),
+      attributions: [
+        '<a href="https://daten.berlin.de/datensaetze/co2-emissionen-durch-anlagen-nach-dem-treibhausgas-emissionshandelsgesetz-tehg-2"' +
+        ' target="_blank">Umweltatlas Berlin / TEHG (dl-de/by-2-0)</a>'
+      ],
+      url: (extent): string => {
+        return 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s08_07_2tehganlagen?service=wfs&' +
+        'version=1.1.0&request=GetFeature&typename=fis:s08_07_2tehganlagen&outputFormat=application/json';
+      },
+      strategy: bboxStrategy
+    });
+
+    // Create style
+    const pointStyle = new Style({
+      image: new Circle({
+        fill: new Fill({
+          color: 'rgba(255,255,255,0.4)'
+        }),
+        stroke: new Stroke({
+          color: 'rgba(0, 0, 255, 1.0)',
+          width: 2
+        }),
+        radius: 5
+      })
+    });
+
+    // Create a vector layer
+    const vectorLayer = new Vector({
+      source: vectorSource,
+      style: pointStyle
+    });
+    console.log(vectorLayer);
 
     this.map = new Map({
       target: this.mapId,
       layers: [
         new TileLayer({
           source: new OSM()
-        })
+        }),
+        vectorLayer
       ],
       view: new View({
         center: fromLonLat([13.4050, 52.5200]),
         zoom: 11
       }),
-      // TODO: sort out the coordinates!
       controls: defaultControls().extend([
         new ZoomToExtent({
           extent: [
-            813079.7791264898, 5929220.284081122,
-            848966.9639063801, 5936863.986909639
+            1418858.226930063, 6854302.831833711,
+            1565617.3212376016, 6935096.770731142
           ]
         })
       ])
